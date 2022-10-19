7a1c21efc77197e1414b44631bbe40b8e591f273
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-19 17:38:43 +0100

    refined the model

--- things/models.py
+++ things/models.py
@@ -4,6 +4,18 @@ from django.db.models import Model
 
 # Create your models here.
 class Things(Model):
-     name = models.CharField()
-     description = models.CharField()
-     quantity = models.IntegerField()
+     name = models.CharField(
+          unique=True,
+          blank=False,
+          max_length=30
+     )
+     description = models.CharField(
+          unique=False,
+          blank=False,
+          max_length=120
+     )
+     quantity = models.IntegerField(
+          unique=False,
+          max_length=100,
+
+     )
