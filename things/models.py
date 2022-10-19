dd0f8c1d8a2f2cb0c670928474800c97a914f987
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-19 17:22:04 +0100

    Created basic model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Model
 
 # Create your models here.
+class Things(Model):
+     name = models.CharField()
+     description = models.CharField()
+     quantity = models.IntegerField()
